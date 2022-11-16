07437f2488b6a307b833b57f021b836c6c13a496
Christine Lin <christinelin868@gmail.com>
2022-11-15 16:51:02 -0800

    working on media queries and implementing a hamburger menu

---
+++ script.js
@@ -0,0 +1,9 @@
+const menu = document.querySelector('.menu');
+const menuItems = document.querySelector('.menu-items')
+const hamburger = document.querySelector('.hamburger')
+const closeIcon = document.querySelector('close-menu-icon')
+const menuIcon = document.querySelector('menu-icon')
+
+// function toggleHamburger() {
+//     if(menu.classList.contains(''))
+// }
